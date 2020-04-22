ce9e12f1d753a95d240cbb90cddc11dde14d0797
IntwalaRiya <47845800+IntwalaRiya@users.noreply.github.com>
2020-04-22 21:50:43 +0530

    Add files via upload

---
+++ Project/Python/Python/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.urls import path
+from register import views
+from django.views.generic import TemplateView
+from django.contrib import admin
+
+app_name = 'Register' # So we can use it like: {% url 'mymodule:user_register' %} on our template.
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^user_login/$',views.user_login,name='user_login'),
+    url(r'^user_home/$',views.user_home,name='user_home'),
+    url(r'^$',views.user_register,name='user_register'),
+]
